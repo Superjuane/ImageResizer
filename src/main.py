1a0b36e8f426151e8cead8d753a5a6cb9980e361
Juane Olivan <juan.emilio.olivan@estudiantat.upc.edu>
2022-06-10 21:07:49 +0200

    Icono

--- src/main.py
+++ src/main.py
@@ -19,40 +19,25 @@ file_list_column = [
     [sg.Button('Convierte la carpeta completa'), ],
 ]
 
-# image_viewer_column = [
-#     [sg.Text("Choose an image from list on left:")],
-#     [sg.Text(size=(40, 1), key="-TOUT-")],
-#     [sg.Image(key="-IMAGE-")],
-# ]
-
 layout = [
     [
         sg.Column(file_list_column),
-        # sg.VSeperator(),
-        # sg.Column(image_viewer_column),
-
     ]
 ]
 
-window = sg.Window("Prepararador de imagenes BIG/SMALL", layout)
+window = sg.Window("Prepararador de imagenes BIG/SMALL",  layout, icon=r'C:\Users\Juane Olivan\Documents\Eugenialazaro.com\repositorioImagenes\sol.ico')
 
 while True:
-    print = sg.Print
     event, values = window.read()
     if event == "Exit" or event == sg.WIN_CLOSED:
         break
 
     if event == "-FOLDER1-":
-        print("folder")
-        print (values["-FOLDER1-"])
         folder = values["-FOLDER1-"]
-        print(folder)
         try:
-            print("try")
             # Get list of files in folder
             file_list = os.listdir(folder)
         except:
-            print("except")
             file_list = []
 
         fnames = [
@@ -61,25 +46,25 @@ while True:
             if os.path.isfile(os.path.join(folder, f))
                 and f.lower().endswith((".jpeg", ".gif", ".jpg", ".png"))
         ]
-        for x in fnames:
-            print(x)
         window["-FILE LIST-"].update(fnames)
 
     if event == 'Convierte la carpeta completa':
         try:
-            ir.imageResizerFolder(values["-FOLDER1-"], "b")
+            ir.imageResizerFolder(values["-FOLDER1-"], "")
         except NameError:
             Warning("Error: No se ha seleccionado ninguna carpeta a procesar")
 
 
     elif event == "-FILE LIST-":  # A file was chosen from the listbox
-
-        try:
-            ir.imageResizerFile(values["-FILE LIST-"][0], values["-FOLDER1-"])
-            print("Path: " + values["-FOLDER1-"])
-            print("file: " + values["-FILE LIST-"][0])
-
-        except NameError:
-            Warning("Error: No se ha seleccionado ninguna foto a convertir")
+        clicked = sg.PopupOKCancel("Quieres procesar la imagen "+values["-FILE LIST-"][0])
+        if clicked == 'OK':
+            try:
+                ir.imageResizerFile(values["-FILE LIST-"][0], values["-FOLDER1-"])
+            except NameError:
+                Warning("Error: No se ha seleccionado ninguna foto a convertir")
+        elif clicked == 'Cancel':
+            pass
+        elif clicked == None:
+            pass
 
 window.close()
